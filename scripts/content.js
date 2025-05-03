c9c090f536981de69369a853a8ab72429f53907b
bubbletea03 <99546167+bubbletea03@users.noreply.github.com>
2025-05-03 23:51:35 +0900

    iframe으로 시간표 탭 가져오기 실험 추가

--- scripts/content.js
+++ scripts/content.js
@@ -13,7 +13,24 @@ const observer = new MutationObserver((mutations, observer) => {
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
                 'Accept-Language': 'ko-KR,ko;q=0.9',
                 'Referer': 'https://everytime.kr/',}
-    }).then(response => console.log(response.text()));
+            }).then(response => console.log(response.text()));
+
+        const iframe = document.createElement('iframe');
+        iframe.src = 'https://everytime.kr/timetable';
+        iframe.style.display = 'none';
+        document.body.appendChild(iframe);
+        
+        iframe.onload = () => {
+            try {
+                const doc = iframe.contentDocument || iframe.contentWindow.document;
+                const data = doc.body;
+                console.log('iframe 안 내용', data);
+            } 
+            catch (err) {
+                console.error('iframe 접근 실패:', err);
+            }
+        }
+
 
         observer.disconnect();
     }
