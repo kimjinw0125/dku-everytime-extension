faec10a82330a9cf3c3a748b1c44aeebe6177fa8
bubbletea03 <99546167+bubbletea03@users.noreply.github.com>
2025-05-03 23:30:14 +0900

    fetch 실험 코드 추가가

--- scripts/content.js
+++ scripts/content.js
@@ -1,2 +1,25 @@
-const logoImg = document.querySelector("nav #logo a img");
-logoImg.src = chrome.runtime.getURL("icons/dku_main_logo.png");
+const observer = new MutationObserver((mutations, observer) => {
+    const logoImg = document.querySelector("nav #logo a img");
+
+    if (logoImg) {
+        console.log("test complete");
+        logoImg.src = chrome.runtime.getURL("icons/dku_main_logo.png");
+
+        fetch('https://everytime.kr/timetable', {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'User-Agent': navigator.userAgent,
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9',
+                'Accept-Language': 'ko-KR,ko;q=0.9',
+                'Referer': 'https://everytime.kr/',}
+    }).then(response => console.log(response.text()));
+
+        observer.disconnect();
+    }
+})
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+})
